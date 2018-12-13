1024e294a89ca1d20e751c02c0549bfc7a2d66fb
simon <simon.jobin@heig-vd.ch>
2018-12-13 22:50:22 +0100

    code barre a tester

--- Labo3_Jobin_Thomas_Lagha/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/BarcodeActivity.java
+++ Labo3_Jobin_Thomas_Lagha/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/BarcodeActivity.java
@@ -1,6 +1,70 @@
 package ch.heigvd.iict.sym.a3dcompassapp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+/* source : https://github.com/journeyapps/zxing-android-embedded */
 public class BarcodeActivity extends Activity {
+
+    private Button scanCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode);
+
+        scanCode = findViewById(R.id.scanCode);
+
+        scanCode.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                scan();
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(final Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    // Get the results:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    protected void scan(){
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Scan a barcode");
+        integrator.setCameraId(0);  // Use a specific camera of the device
+        integrator.setBeepEnabled(true);
+        integrator.initiateScan();
+
+    }
 }
